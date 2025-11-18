00ca11d80040c16b43282b3e1597ee81dce25809
章魚 <126858934+braces33@users.noreply.github.com>
2025-11-18 15:14:58 +0800

    Implement money calculation based on hours worked

---
+++ money.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class money {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float hour = scan.nextFloat(); 
+        float money ;
+        if (hour <= 60){
+            money = hour * 120;
+        }
+        else if (hour > 60 && hour <= 80){ 
+            money = (hour - 60) * 120 * 1.25f + 60 * 120; 
+        }
+        else if (hour > 80){
+            money = ((hour - 60) * 120 * 1.5f) + (60 * 120); 
+        } else {
+             money = 0.0f;
+        }
+        System.out.print(money);
+    }
+}
